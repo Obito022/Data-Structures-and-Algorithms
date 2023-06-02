8a734750ce7ac8b8a6c752d065963db2d211acf3
obito022 <abrar15022@gmail.com>
2023-06-03 02:43:54 +0600

    o generating subsets based on the bit representation of integers

---
+++ Generating subsets(using Bit Masking).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generateSubsets(vector<int> &set) {
+  int n = set.size();
+  vector<vector<int>> subsets;
+  for (int b = 0; b < (1 << n); b++) {
+    vector<int> subset;
+    for (int i = 0; i < n; i++) {
+      if (b & (1 << i)) subset.push_back(set[i]);
+    }
+    subsets.push_back(subset);
+  }
+  return subsets;
+}
+
+
+int main() {
+
+  vector<int> set = {2, 4, 5};
+
+  vector<vector<int>> subsets = generateSubsets(set);
+
+  for (auto subset : subsets) {
+    for (auto element : subset) {
+      cout << element << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
